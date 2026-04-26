b423ca0776b8fff3a6f53afa6a9d475558b2f4fa
Wishduck <github@johnrosshiggins.com>
2026-04-25 21:52:42 -0700

    First beta selection

---
+++ preload.lua
@@ -0,0 +1,9 @@
+local mod = game.mod_runtime[game.current_mod]
+local storage = game.mod_storage[game.current_mod]
+
+mod.storage = storage
+
+game.mapgen_functions["crt_lab_veh_house"] = function(...) return mod.crt_lab_veh_house.draw(...) end
+game.mapgen_functions["crt_lab_veh_hall"] = function(...) return mod.crt_lab_veh_hall.draw(...) end
+game.mapgen_functions["crt_lab_veh_main"] = function(...) return mod.crt_lab_veh_main.draw(...) end
+game.add_hook("on_creature_melee_attacked", function(...) return mod.on_creature_melee_attacked(...) end)
